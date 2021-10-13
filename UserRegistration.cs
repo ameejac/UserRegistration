d1e7908f038f357db44d26b1a54b0e045810d3a7
ameejac <ameejacherukuru123@gmail.com>
2021-10-13 10:00:03 -0700

    [added: verify email is valid or not]

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -14,18 +14,20 @@ namespace UserRegistrationDemo
         //string pattern = "^[A-Z]{1}[a-z]{2,15}$";
 
         //uc2 FirstName and user needs to add the LastName
-        string pattern = "^[A-Z]{1}[a-z]{2,30}[ ]*[A-Z]{1}[a-z]{2,20}$";
+        //string pattern = "^[A-Z]{1}[a-z]{2,30}[ ]*[A-Z]{1}[a-z]{2,20}$";
 
+        //uc3 email Address
+        string pattern = "^[a-zA-Z0-9]+[._+]{0,1}[a-zA-Z0-9]*@[a-zA-Z0-9]{1,15}.[a-zA-Z]{2,10}[.]*[a-zA-Z]*$";
         //Validating examples
-        string[] inputs = { "Jaan Cherukuru","Xyz Abc", "jaan cherukuru", "Ameeja Cherukuru", "Abcdef abcsd","reshma cherurukuru", "Reshma Cherukuru", "touhid cherukuru"};
-
+        //string[] inputs = { "Jaan Cherukuru","Xyz Abc", "jaan cherukuru", "Ameeja Cherukuru", "Abcdef abcsd","reshma cherurukuru", "Reshma Cherukuru", "touhid cherukuru"};
+        string[] inputs = { "ameejacherukuru123@gmail.com","ameeja2.com","ameejac2","@ameeja.com","AMEEJACHERUKURU@GMAIL.COM","ameejacherukuru123a@gamil.com" };
         /// <summary>
         /// Verifying the process
         /// </summary>
         public void Validating()
         {
             Regex regex = new Regex(pattern);
-            Console.WriteLine("Verifying  FirstName starts with capital: ");
+            Console.WriteLine("Verifying  Email Address: ");
             IterationLoop(inputs, regex);
         }
         public static void IterationLoop(string[] arr, Regex regex)
