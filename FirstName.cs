9e1ce8d3fbcf3a055d9f3cd31c2f204b3024feb2
ameejac <ameejacherukuru123@gmail.com>
2021-10-13 08:39:15 -0700

    ameeja[added: FirstName start with capital letters]

---
+++ FirstName.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationDemo
+{
+    class UserRegistration
+    {
+        //Uc1 FirstName
+        //writing the valid pattern
+        string pattern = "^[A-Z]{1}[a-z]{2,15}$"; 
+                                                 
+        //Validating examples
+        string[] inputs = { "Jaan","Xyz", "jaan", "Ameeja", "Abcdef","ameena", "Rehan", "rehan"};
+
+        /// <summary>
+        /// Verifying the process
+        /// </summary>
+        public void Validating()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Verifying  FirstName starts with capital: ");
+            IterationLoop(inputs, regex);
+        }
+        public static void IterationLoop(string[] arr, Regex regex)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool result = regex.IsMatch(arr[i]);
+                if (result)
+                {
+                    Console.WriteLine(arr[i] + " It is --->" + "Valid");
+                }
+                else
+                {
+                    Console.WriteLine(arr[i] + " It is --->" + "Invalid");
+                }
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserRegistrationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a object.
+            UserRegistration sample = new UserRegistration();
+            sample.Validating();
+            Console.ReadLine();
+
+        }
+    }
+}
